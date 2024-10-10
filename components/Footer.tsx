3a62a9f4fe31d725879895535c6b828fb839678e
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2024-10-10 14:12:33 +0430

    clean up

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 
 export default function Footer() {
   return (
-    <View style={{ marginBottom: 10, backgroundColor: "#F4CE14" }}>
+    <View style={{ marginBottom: 2, backgroundColor: "#F4CE14" }}>
       <Text
         style={{
           fontSize: 18,
